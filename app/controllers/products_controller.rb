931ffbee14545870d09a4005947f5cd813459457
vishal tyagi <vishaltyagi.tyagi444@gmail.com>
2017-02-27 15:59:24 +0530

    hghj

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -37,47 +37,41 @@ class ProductsController < ApplicationController
 
   # POST /products
   # POST /products.json
-  def create
-      # browser = Watir::Browser.new :firefox 
-      @search = params[:search]
-      client = Mechanize.new
+def create
+  if params[:search] && params[:keyword].present?
+    @search_keyword = params[:search].split(',')- [""]
+    # if @search_keyword.size > 1
+    @search_keyword.each do |ks|
 
-          @title = []
-          @url = []
-          @discription = []
-      (1..531).step(10) do |p|
-        client.get("https://www.bing.com/search?q=#{@search}&first=#{p}") do |page|
-
-          document = Nokogiri::HTML::Document.parse(page.body)
-          alldata = document.css('li.b_algo') rescue ""
-
-          alldata.each do |ad|
-            @title << ad.at('a').text  rescue ""
-            @discription << ad.at('p').text rescue ""
-            @url << ad.at('a')['href'].to_s  rescue ""
-          end
-        end
-      end
-
-      @product = Product.create(title: @title, description: @discription, url: @url, search: @search)
-
-      # CSV.open("#{Rails.root}/public/uploads/reports/#{@product.id}.csv", "wb") do |csv| 
-      # csv << ["Title", "Discription", "Url"] 
-      # (0..@title.length).each do |index| 
-      # csv << [@title[index], @discription[index], @url[index]] 
-      # end 
-      # end 
-
-
-    # format.csv do
-    #   response.headers['Content-Type'] = 'text/csv'
-    #   response.headers['Content-Disposition'] = 'attachment; filename=file.csv'    
-    #   render :template => "/home/vishal/Downloads/file.csv"
-    # end
-      redirect_to products_path
+      @serach_k = ks
+      searching(@serach_k)
+      @product = Product.create(title: @title, description: @discription, url: @url, search: @serach_k)
+    end
+  # end
+  else
+    @search = params[:search]
+    searching(@search)
+    @product = Product.create(title: @title, description: @discription, url: @url, search: @search)
   end
+  redirect_to products_path
+  end
+end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
- 
+def searching(search)
+
+  client = Mechanize.new
+  @title = []
+  @url = []
+  @discription = []
+  (1..531).step(10) do |p|
+    client.get("https://www.bing.com/search?q=#{search}&first=#{p}") do |page|
+      document = Nokogiri::HTML::Document.parse(page.body)
+      alldata = document.css('li.b_algo') rescue ""
+      alldata.each do |ad|
+        @title << ad.at('a').text  rescue ""
+        @discription << ad.at('p').text rescue ""
+        @url << ad.at('a')['href'].to_s  rescue ""
+      end
+    end
+  end
 end
